60faf5f5718e544201160eda377f70b6a10a99b7
Vedant Nandoskar <vedant.nandoskar@gmail.com>
2025-01-22 01:06:57 -0600

    add site path

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/grant-management-system/',
   plugins: [
     vue(),
     vueJsx(),
